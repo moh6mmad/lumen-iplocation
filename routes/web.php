1cba9cd7325e7f0b7634784de2057c5f78229cdf
Mohammad <mohammad@lync.me>
2022-04-03 16:02:20 +0200

    Added register route to web router

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,8 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->post('register', 'AuthController@register'); 
+ });
 
 $router->get('/', 'LocationController@index');
